a071ec27ac021f70df89048b6332d2d5409a82c9
VaRsha-76 <67176820+VaRsha-76@users.noreply.github.com>
2020-07-24 08:51:16 +0530

    Delete main (1).py

--- main (1).py
+++
@@ -1,6 +0,0 @@
-﻿hour = int(input("Starting time (hours): "))
-mins = int(input("Starting time (minutes): "))
-dura = int(input("Event duration (minutes): "))
-
-# put your code here
-print("Finish Time:",((hour+(mins+dura)//60)%24),":",(mins+dura)%60)
